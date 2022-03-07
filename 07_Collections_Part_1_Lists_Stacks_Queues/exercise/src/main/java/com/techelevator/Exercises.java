ac0267f83c39656258b9b074f73c8b76f15d49be
eckoup <eckoup@gmail.com>
2022-03-06 19:52:55 -0500

    Collections Part 1

--- 07_Collections_Part_1_Lists_Stacks_Queues/exercise/src/main/java/com/techelevator/Exercises.java
+++ 07_Collections_Part_1_Lists_Stacks_Queues/exercise/src/main/java/com/techelevator/Exercises.java
@@ -23,7 +23,7 @@ public class Exercises {
 		}
 		return array2List;
 	}
-
+//test
 	/*
 	 Given a list of Strings, return an array containing the same Strings in the same order
 	 list2Array( ["Apple", "Orange", "Banana"] )  ->  {"Apple", "Orange", "Banana"}
