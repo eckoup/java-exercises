9c62fe07b586c84a69b940f170002b5faf827e5f
eckoup <eckoup@gmail.com>
2022-03-25 23:29:30 -0400

    Managing Inheritance Exercise: Paint Calculator

---
+++ 13_Managing_Inheritance/exercise/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+public class RectangleWall extends Wall {
+    private int length;
+    private int height;
+
+    public RectangleWall(String name, String color, int length, int height ) {
+        super(name, color);
+        this.length = length;
+        this.height = height;
+    }
+       public int getArea() {
+        return length * height;
+    }
+    public int getLength(){
+        return length;
+    }
+    public int getHeight(){
+        return height;
+    }
+   public String toString(){
+    return this.getName() + " ("+length + "x" + height+") " + "rectangle";
+    }
+}
+
