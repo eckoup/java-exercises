c74788b2fb626e88d57929c7ab0787b5775cbf41
eckoup <eckoup@gmail.com>
2022-04-03 20:41:40 -0400

    FizzBuzz Writer & Find and Replace Program

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,46 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the search word");
+        String search = scanner.nextLine();
+        System.out.println("What is the replacement word");
+        String replace = scanner.nextLine();
+        System.out.println("What is the source file?");
+        String filename = scanner.nextLine();
+        //path to source file
+        System.out.println("What is the destination file?");
+        String destination = scanner.nextLine();
+        //path to destination file
+        try{
+            FindAndReplace.FindReplace(search, replace, filename, destination);
+            }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    public static void FindReplace(String searchWord, String replaceWord, String replaceFile, String destinationFile) throws FileNotFoundException {
+        File dataFile = new File(replaceFile);
+        File destination = new File(destinationFile);
+        Scanner datain = new Scanner(dataFile);
+        PrintWriter dataOutput = new PrintWriter(destination);
+
+        while (datain.hasNextLine()) {
+            String lineOfInput = datain.nextLine();
+            String newLine = lineOfInput.replaceAll(searchWord, replaceWord);
+            dataOutput.println(newLine);
+        }
+        dataOutput.close();
+    }
 }
+
+
--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,40 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What's the file path homie?");
+		String filePath = scanner.nextLine();
+try{
+	FizzWriter.FizzyBoi(filePath);
+} catch (FileNotFoundException e){
+	System.err.println("File not found silly, try again");
+}
+	}
+public static void FizzyBoi(String filePath) throws FileNotFoundException {
+		File fizzFile = new File(filePath);
+	PrintWriter dataOutput = new PrintWriter(fizzFile);
+	for (int n = 1; n <=300; n++){
+		if (n%3==0 && n%5==0 ){
+			dataOutput.println("FizzBuzz");
+		}
+		else if ( n%3==0){
+			dataOutput.println("Fizz");
+		}
+		else if (n%5==0){
+			dataOutput.println("Buzz");
+		}
+		else{
+			dataOutput.println(n);
+		}
 
 	}
-
+	dataOutput.close();
+}
 }
